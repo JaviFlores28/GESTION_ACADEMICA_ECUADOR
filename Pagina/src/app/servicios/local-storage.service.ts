4faa8ca19f64a635bd60ea01410d535ea307503f
Andres Villacres <andreslkf14@gmail.com>
2023-10-20 19:45:35 -0500

    componentes tabla modal ...

--- Pagina/src/app/servicios/local-storage.service.ts
+++ Pagina/src/app/servicios/local-storage.service.ts
@@ -9,15 +9,15 @@ export class LocalStorageService {
   constructor() { }
 
   setItem(value: any) {
-    localStorage.setItem(params.KEY_ENCRYPT, btoa(JSON.stringify(value)));
+    localStorage.setItem(params.KEY_NAME, btoa(JSON.stringify(value)));
   }
 
   getItem(): any {
-    const item = localStorage.getItem(params.KEY_ENCRYPT);
+    const item = localStorage.getItem(params.KEY_NAME);
     return item ? JSON.parse(atob(item)) : null;
   }
 
   removeItem() {
-    localStorage.removeItem(params.KEY_ENCRYPT);
+    localStorage.removeItem(params.KEY_NAME);
   }
 }
