fc9e8ac46c64d4557c5f67129f140b32896705ff
Javier Flores <javi.2903@outlook.es>
2023-10-24 13:25:34 -0500

    fusion andres

--- Pagina/src/app/modelos/interfaces/profesor.interface.ts
+++ Pagina/src/app/modelos/interfaces/profesor.interface.ts
@@ -6,14 +6,4 @@ export interface Profesor {
     ESTADO: string;
     CREADOR_ID: string;
     FECHA_CREACION?:Date | undefined;
-    
-    /*constructor(PRF_ID: string, PRF_FECH_INGR_INST: Date, PRF_FECH_INGR_MAG: Date, USR_ID: string, ESTADO: string, CREADOR_ID: string, FECHA_CREACION?: Date | undefined) {
-        this.PRF_ID = PRF_ID;
-      this.PRF_FECH_INGR_INST = PRF_FECH_INGR_INST;
-      this.PRF_FECH_INGR_MAG = PRF_FECH_INGR_MAG;
-      this.USR_ID = USR_ID;
-      this.ESTADO = ESTADO;
-      this.CREADOR_ID = CREADOR_ID;
-      this.FECHA_CREACION = FECHA_CREACION;
-    }*/
 }
