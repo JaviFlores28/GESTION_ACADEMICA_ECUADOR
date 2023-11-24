bebacbe1fbe5aab6b9818f0386436297c4443424
Javier Flores <javi.2903@outlook.es>
2023-11-24 10:28:15 -0500

    modificando archivos

--- Servidor/src/test.ts
+++ Servidor/src/test.ts
@@ -1,13 +1,12 @@
 //import AnioLectivoNegocio from './Negocio/AnioLectivoNegocio'; // Asegúrate de proporcionar la ruta correcta al archivo
 import UsuarioNegocio from './Negocio/UsuarioNegocio'; // Asegúrate de proporcionar la ruta correcta al archivo
-
-import UsuarioEntidad from './Entidades/UsuarioEntidad';
+import UsuarioDatos from './datos/UsuarioDatos';
 
 async function main() {
   try {
     const anioLectivoNegocio = new UsuarioNegocio();
     //const anioLectivo = new AnioLectivo('12', 'cambio81', new Date(), new Date(), 1, 1, 1, 1, 1, 1, 1, 1, '1', '1'); // Asegúrate de proporcionar los datos del AnioLectivo
-    const usuario = new UsuarioEntidad('66c85577-59a5-42d7-9a53-a7c4c73f15e3', 'cambio', '', '', '', '', '', '', '', '', new Date(), '', '', '', 1, 1, 1, '1');
+    const usuario = null;
     const serializedObject = JSON.stringify(usuario);
     // const result2 = await anioLectivoNegocio.obtenerAnioLectivos()
     console.log(serializedObject);
